8fe62cc03428b610fd7f77d7087491571c37653b
Evgeniy Zlobina <ZEA_6590@mail.ru>
2023-05-24 20:26:55 +0300

    Class FormDate

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        Post.surname = "Иванов";
+        post.name = "Иван";
+        post.subsciption = "Иванович";
+        post.passport = "1234 123456";
+        post.phone = "89828982982";
+        post.patronymie = "зарегистрирован";
+    }
+}
---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Post {
+        public static String surname; // поле для фамилии
+        public String name; //поле для имени
+        public String patronymie; // поле для отчества
+        public String passport; // поле для паспорта
+        public String phone; // поле для телефона
+        public String subsciption; // поле о регистрации
+    }
+
