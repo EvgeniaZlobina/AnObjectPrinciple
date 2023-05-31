e569ae22160de1ce65a5f299464f709c1eaa22db
Evgeniy Zlobina <ZEA_6590@mail.ru>
2023-05-31 20:46:25 +0300

    Reformat Code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,16 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Post post = new Post();
-        Post.surname = "Иванов";
+
         post.name = "Иван";
+        post.surname = "Иванов";
         post.patronymie = "Иванович";
         post.subsciption = true;
         post.passport = "1234 123456";
         post.phone = "89828982982";
-        Post.birthday = new FormDate(); // класс даты рождения
-        Post.birthday.day = 13;
-        Post.birthday.month = 02;
-        Post.birthday.year = 1998;
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 02;
+        post.birthday.year = 1998;
     }
 }
--- src/main/java/org/example/Post.java
+++ src/main/java/org/example/Post.java
@@ -1,14 +1,14 @@
 package org.example;
 
 public class Post {
-    public static FormDate birthday;
-    public static String surname; // поле для фамилии
+    public FormDate birthday;
+    // public static FormDate birthday;
+    //public static String surname; // поле для фамилии
     public String name; //поле для имен
+    public String surname;
     public String patronymie; // поле для отчества
     public String passport; // поле для паспорта
     public String phone; // поле для телефона
     public boolean subsciption; // поле о регистрации
-
-    //public FormDate birthday;
 }
 
